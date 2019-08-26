e048ed1b899f729fec2deabefe1a626c12b2f289
Tommy Campbell <tjc2187@gmail.com>
2019-08-25 21:49:16 -0500

    fixed issue where errors wont display

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -43,8 +43,6 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
         if (errors.hasErrors()) {
-            model.addAttribute(new JobForm());
-            System.out.println("\n\n\n\n" + errors.toString() + "\n\n\n\n");
             return "new-job";
         }
         System.out.println("\n\n\n\n" + jobForm.getName());
